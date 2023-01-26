7887284cfe22a5d8141c50d89db831f645993c9f
usmannaveed896 <usmann207@gmail.com>
2023-01-26 10:02:51 +0500

    initial

---
+++ lib/keyboards.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'keyboards_buttons.dart';
+import 'keyboards_rows.dart';
+
+
+class Keyboard extends StatelessWidget {
+  Keyboard({required this.onTap, required this.keyboardSigns});
+
+  final CallbackButtonTap onTap;
+  final List keyboardSigns;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: keyboardSigns.map((signs) {
+        return KeyboardRows(
+          rowsButtons: signs,
+          onTap: onTap,
+        );
+      }).toList(),
+    );
+  }
+}
